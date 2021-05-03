1bde682358c027e35090470e1cf64d37f620c8b6
Jackie Qiu <jqi26@uclive.ac.nz>
2021-05-03 23:24:08 +1200

    Adding most of the location code from the tutorial. Still figuring out where to put the code when we are using Fragments. Still need to get an API key.

--- app/src/main/java/com/example/parcmarc/Park.kt
+++ app/src/main/java/com/example/parcmarc/Park.kt
@@ -3,12 +3,12 @@ package com.example.parcmarc
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.android.gms.maps.model.LatLng
 
 @Entity(tableName = "park")
 class Park(
     @ColumnInfo var name: String,
-    @ColumnInfo var latitude: Double,
-    @ColumnInfo var longitude: Double,
+    @ColumnInfo var location: LatLng
 ) {
 
     @PrimaryKey(autoGenerate = true) var id: Long = 0
